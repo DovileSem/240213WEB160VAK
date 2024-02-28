147ec093eaa9b275f94542f09eadfbbd6d9a140d
jusu vartotojo vardas <dovile.semaskiene@gmail.com>
2024-02-28 22:02:14 +0200

    JS pradmenys

---
+++ 10_paskaita_JS/script/script.js
@@ -0,0 +1,9 @@
+// Išvedam tekstą į konsolę
+
+console.log('Sveiki as is script.js failo!');
+
+// Išvedame tekstą iššokusiame langelyje
+// alert('Kazkoks tekstas!');
+
+// Išvedame tekstą HTML body laukelyje
+document.write('Tekstas dokumente')
